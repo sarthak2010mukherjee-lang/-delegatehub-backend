763d355a62cb225382a9a0c26d5c915b1e0ecf03
Sarthak Mukherjee <sarthakmukherjee@Sarthaks-MacBook.local>
2026-04-27 19:40:09 +0530

    add root route

--- app.py
+++ app.py
@@ -588,3 +588,6 @@ if __name__ == '__main__':
     app.run(debug=True, port=5001, host='0.0.0.0')
 port = int(os.environ.get("PORT", 5001))
 app.run(host="0.0.0.0", port=port)
+@app.route("/")
+def home():
+    return "Backend is running 🚀"
