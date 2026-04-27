04523db61f06c0395737fd01eff88941c379bdd7
Sarthak Mukherjee <sarthakmukherjee@Sarthaks-MacBook.local>
2026-04-27 19:28:18 +0530

    add gunicorn

--- app.py
+++ app.py
@@ -586,3 +586,5 @@ def ai_country():
         return jsonify({"error": str(e)}), 500
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='0.0.0.0')
+port = int(os.environ.get("PORT", 5001))
+app.run(host="0.0.0.0", port=port)
